f7c2b966abcc656ca53cc0ef4cedb57f3ad7cde6
SaintDesire <95407889+SaintDesire@users.noreply.github.com>
2022-10-02 18:13:31 +0300

    Add files via upload

---
+++ OP_Lab2/Phone.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telephone
+{
+    partial class Phone
+    {
+        const string Type = "phone"; // поле константа
+        public static readonly int NumberForHesh;
+        public int Id { get; set; }
+        public String? Name { get; set; }
+        public String? Surname { get; set; }
+        public String? Patronymic { get; set; }
+        public String? Adress { get; set; }
+        public String? CardNumber { get; set; }
+
+        public int hours;
+        public int minutes;
+
+        public bool CallType;
+        public int Debet { get; set; }
+        public int Credit { get; set; }
+        public int kol_voObject = 0;
+
+        public Phone()
+        {
+            Id = 1;
+            Name = "Александр";
+            Surname = "Питерский";
+            Patronymic = "Олегович";
+            Adress = "Улица Пушкина";
+            CardNumber = "5470 8740 4560 2388";
+            Debet = 23516;
+            Credit = 2380;
+            CallType = false;
+            hours = 2;
+            minutes = 15;
+            kol_voObject++;
+        }
+
+        public Phone(string name, string surname, string patronymic, string cardNumber, int hours, int minutes)
+        {
+            Id = 2;
+            this.Name = name;
+            this.Surname = surname;
+            this.Patronymic = patronymic;
+            Adress = "Улица Пушкина";
+            this.CardNumber = cardNumber;
+            Debet = 23516;
+            Credit = 2380;
+            CallType = true;
+            this.hours = hours;
+            this.minutes = minutes;
+            kol_voObject++;
+        }
+
+        public Phone(string name, string surname, string patronymic, string cardNumber, int hours, int minutes, string adress, int debet, int credit)
+        {
+            Id = 3;
+            this.Name = name;
+            this.Surname = surname;
+            this.Patronymic = patronymic;
+            this.Adress = adress;
+            this.CardNumber = cardNumber;
+            this.Debet = debet;
+            this.Credit = credit;
+            this.hours = hours;
+            this.minutes = minutes;
+            CallType = true;
+            kol_voObject++;
+        }
+
+        public Phone(int id, string name, string surname, string patronymic = " ")
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Surname = surname;
+            this.Patronymic = patronymic;
+            Adress = "Улица Колотушкина";
+            CardNumber = "1234 5678 9101 1121";
+            Debet = 56712;
+            Credit = 12341;
+            CallType = true;
+            hours = 1;
+            minutes = 46;
+            kol_voObject++;
+        }
+
+        public override int GetHashCode()
+        {
+            Random x = new Random();
+            return Id * 3 * x.Next(100) * NumberForHesh;
+        }
+
+        static Phone() { NumberForHesh = 10; }
+        public Phone(ref int Debet, out int? take)
+        {
+            Id = Debet;
+            Id = this.GetHashCode();
+            take = 14;
+        }
+        protected float[] msf = new float[10];
+        public float this[int j]
+        {
+            get
+            {
+                return msf[j];
+            }
+            set
+            {
+                msf[j] = value;
+            }
+        }
+        public static void ClassInfo()
+        {
+            Console.WriteLine("Статическая информация о классе");
+            Console.WriteLine("Это класс телефон");
+            Console.WriteLine("Его тип: " + Type);
+            Secret();
+        }
+        private static void Secret()
+        {
+            Console.WriteLine("Закрытый конструктор");
+        }
+        public void TotalAmountOfClients()
+        {
+            Console.WriteLine($"Общее кол-во телефонов {kol_voObject}");
+        }
+        static void Menu()
+        {
+            Console.WriteLine("Ввод данных");
+
+        }
+        public void Interface()
+        {
+
+            Menu();
+            Console.WriteLine("Ввести фамилию");
+            Surname = Console.ReadLine();
+            Console.WriteLine("Ввести имя");
+            Name = Console.ReadLine();
+            Console.WriteLine("Ввести отчество");
+            Patronymic = Console.ReadLine();
+            Console.WriteLine("Ввести адрес");
+            Adress = Console.ReadLine();
+            Console.WriteLine("Ввести номер карты");
+            CardNumber = Console.ReadLine();
+            Console.WriteLine("Ввести дебет");
+            Debet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ввести кредит");
+            Credit = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine(Surname + " " + Name + " " + Patronymic + " | ФИО");
+            Console.WriteLine(Adress + " | адрес");
+            Console.WriteLine(CardNumber + " | номер карты");
+            Console.WriteLine(Debet + " | дебет");
+            Console.WriteLine(Credit + " | кредит");
+            Console.WriteLine(Id + " | ID");
+        }
+        public void ShowBalance()
+        {
+            Console.WriteLine("Баланс на текущий момент: " + Debet);
+        }
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+        //override - Если было использовано это ключевое слово, на выходе будет использоваться переопределенный метод. Вне зависимости от того, как было оформлено обращение к методу.
+    }
+}
---
+++ OP_Lab2/Program.cs
@@ -0,0 +1,127 @@
+﻿//Вариант 8
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace telephone
+{
+    static class House
+    {
+        static int Main()
+        {
+            bool end = false;
+            int choice;
+            int mass_elem;
+            int mass_lenght = 4;
+            string? findName;
+            int NumberForID = 12;
+            int? takecol;
+            (int? hours, int? minutes) AllowedTime;
+            telephone.Phone[] PhoneMassive = new telephone.Phone[100];
+
+            Phone phone1 = new Phone();
+            Phone phone2 = new Phone("Олег", "Борисов", "Анатольевич", "1234 5632 2312 5156", 3, 19);
+            Phone phone3 = new Phone("Анатолий", "Боярский", "Валерьевич", "9123 5162 3216 9312", 0, 27, "Улица Петрова", 92700, 12349);
+            Phone phone4 = new Phone(4, "Николай", "Насолях");
+
+            PhoneMassive[0] = phone1;
+            PhoneMassive[1] = phone2;
+            PhoneMassive[2] = phone3;
+            PhoneMassive[3] = phone4;
+            /*  
+              tovarka.Product obj = new tovarka.Product(ref NumberForID, out takecol);
+              for (int i = 0; i < 10; i++)
+              {
+                  obj[i] = (float)1.5 * i;
+              }
+              for (int i = 0; i < 10; i++)
+              {
+                  Console.WriteLine("{0}", obj[i]);
+              }
+            пример как работает индексатор
+            */
+            do
+            {
+                Console.WriteLine("Главное меню\n" +
+                    "1)Доавить телефон\n" +
+                    "2)Посмотреть информацию о телефоне\n" +
+                    "3)Посмотреть текущий баланс\n" +
+                    "4)Cписок абонентов с превышеным временем внутригородских звонков\n" +
+                    "5)Список абонентов которые пользовались междугородней связью\n" +
+                    "6)Информация о классе\n"
+                    );
+                Console.WriteLine("\n");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        PhoneMassive[mass_lenght] = new telephone.Phone(ref NumberForID, out takecol);
+                        PhoneMassive[mass_lenght].Interface();
+                        mass_lenght++;
+                        Console.WriteLine("\n");
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите номер телефона в массиве");
+                        mass_elem = Convert.ToInt32(Console.ReadLine());
+                        if (mass_lenght > mass_elem)
+                        {
+                            PhoneMassive[mass_elem-1].ShowInfo();
+                        }
+                        else
+                            Console.WriteLine("Error\n");
+                        Console.WriteLine("\n");
+                        break;
+                    case 3:
+                        Console.WriteLine("Введите номер продукта в массиве");
+                        mass_elem = Convert.ToInt32(Console.ReadLine());
+                        if (mass_lenght > mass_elem)
+                        {
+                            PhoneMassive[mass_elem].ShowBalance();
+                        }
+                        else
+                            Console.WriteLine("Error\n");
+                        Console.WriteLine("\n");
+                        break;
+                    case 4:
+                        int hours, minutes;
+                        
+                        Console.WriteLine("Введите разрешенное кол-во часов");
+                        hours = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите разрешенное кол-во минут");
+                        minutes = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < mass_lenght; i++)
+                        {
+                            if (PhoneMassive[i].hours > hours && PhoneMassive[i].minutes > minutes)
+                            {
+                                Console.WriteLine("Превышено время внутригородских звонков");
+                                PhoneMassive[i].ShowInfo();
+                            }
+                        }
+                        Console.WriteLine("\n");
+                        break;
+                    case 5:
+                        Console.WriteLine("\nПользовались междугородней связью\n");
+                        for (int i = 0; i < mass_lenght; i++)
+                        {
+                            if (PhoneMassive[i].CallType == true)
+                            {
+                                PhoneMassive[i].ShowInfo();
+                                Console.WriteLine("\n");
+                            }
+                        }
+                        break;
+                    case 6:
+                        if (mass_lenght != 0)
+                            telephone.Phone.ClassInfo();
+                        Console.WriteLine("\n");
+                        break;
+                    default:
+                        break;
+                }
+            } while (!end);
+            return 0;
+        }
+    }
+}
